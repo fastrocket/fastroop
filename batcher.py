0cd75021fc5db633e68a53b119a5c1fcf0878204
fast rocket <fastrocket@gmail.com>
2023-12-23 02:41:00 -0700

    many-faces pre delete folders

--- batcher.py
+++ batcher.py
@@ -14,6 +14,18 @@ def get_files_in_dir(dir_path):
 
 def run_face_swapper(target_dir, source_dir, output_dir, execution_provider, frame_processor):
   """Runs the face swapper on all files in the target and source directories."""
+
+  # delete the temp directory in the target_dir folder, use the variable target_dir to get the path. Check if it exists first before deleting.
+  temp_dir = os.path.join(target_dir, "temp")
+  if os.path.exists(temp_dir):
+    print(f">>> Deleting directory {temp_dir}")
+    # delete using both Windows and Linux. Do not use "rm -rf" because it will not work on Windows. Do not use "rmdir". Use a Pythonic way to delete a directory.
+
+
+
+
+
+
   for target_file in get_files_in_dir(target_dir):
     for source_file in get_files_in_dir(source_dir):
       # Concatenate variables into a filename like {source}-{target}.mp4
@@ -25,9 +37,9 @@ def run_face_swapper(target_dir, source_dir, output_dir, execution_provider, fra
         continue
 
       # Run the face swapper
-      print(f"python run.py --target {target_file} --source {source_file} -o {output_filename} --execution-provider {execution_provider} --frame-processor {frame_processor} --similar-face-distance 1000")
+      print(f"python run.py --target {target_file} --source {source_file} -o {output_filename} --execution-provider {execution_provider} --frame-processor {frame_processor} --similar-face-distance 1000 --many-faces")
       # print(f"python run.py --target {target_file} --source {source_file} -o {output_filename} --execution-provider {execution_provider} --frame-processor {frame_processor} --temp-frame-quality 100 --similar-face-distance 1000")
-      os.system(f"python run.py --target {target_file} --source {source_file} -o {output_filename} --execution-provider {execution_provider} --frame-processor {frame_processor} --similar-face-distance 1000")
+      os.system(f"python run.py --target {target_file} --source {source_file} -o {output_filename} --execution-provider {execution_provider} --frame-processor {frame_processor} --similar-face-distance 1000 --many-faces")
       # os.system(f"python run.py --target {target_file} --source {source_file} -o {output_filename} --execution-provider {execution_provider} --frame-processor {frame_processor} --temp-frame-quality 100 --similar-face-distance 1000")
 
 # Example usage:
